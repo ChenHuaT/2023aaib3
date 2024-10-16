c9e7d6dfd81e39f5759cb65d141400b596984f6d
ChenHuaT <tom940711@gmail.com>
2024-10-16 11:33:11 +0800

    week06

---
+++ week06/week06-2a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    int now;
+    while( cin >> now){
+        a.push_back(now);
+        int N = a.size();
+
+    }///在小黑裡輸入時，結束全部輸入 Ctrl-Z 加 Enter
+}
+
